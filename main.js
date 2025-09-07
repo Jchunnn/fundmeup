180ee7254e50514ed6239e0ca5791d0ec46e2355
jchunnn <joannechou92@gmail.com>
2025-09-07 12:52:54 +0800

    新增dropdown

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log('Hello world');
+const loginBtn = document.getElementById("loginBtn");
+const subscribeBtn = document.getElementById("subscribeBtn");
+const personalInfo = document.getElementById("personalInfo");
+
+console.log("loginBtn", loginBtn);
+console.log("subscribeBtn", subscribeBtn);
+console.log("personalInfo", personalInfo);
+
+loginBtn.addEventListener("click", () => {
+  // 1. 隱藏登入按鈕
+  loginBtn.style.display = "none";
+
+  // 2. 改變立即訂閱狀態
+  subscribeBtn.classList.remove("btn-outline-primary-600");
+  subscribeBtn.classList.add("btn-primary-600");
+
+  // 3. 顯示personalInfo區塊
+  personalInfo.classList.add("d-lg-flex", "align-items-center");
+});
