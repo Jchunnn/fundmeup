a4375e8d8757a74a2153dc75de26b1ec6ac62d68
jchunnn <joannechou92@gmail.com>
2025-10-05 23:02:00 +0800

    Updates

---
+++ assets/loginpage-CGOJ3u6B.js
@@ -0,0 +1,1 @@
+import"./bootstrap.min-CeTUnxjR.js";const n=document.getElementById("loginPageLoginBtn");console.log("loginPageLoginBtn",n);console.dir(n);n.addEventListener("click",()=>{localStorage.setItem("justLoggedIn","true"),window.location.href="index.html"});
